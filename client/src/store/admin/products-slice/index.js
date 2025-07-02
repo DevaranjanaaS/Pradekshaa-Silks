c519f93a68185d96688cef94b55ca6d71d0f6d66
ayubalikhan <ayubalikhan.j@gmail.com>
2025-07-02 12:07:10 +0530

    http error solved

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -10,7 +10,7 @@ export const addNewProduct = createAsyncThunk(
   "/products/addnewproduct",
   async (formData) => {
     const result = await axios.post(
-      "http://pradekshaa-silks-backend.onrender.com/api/admin/products/add",
+      "https://pradekshaa-silks-backend.onrender.com/api/admin/products/add",
       formData,
       {
         headers: {
@@ -27,7 +27,7 @@ export const fetchAllProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async () => {
     const result = await axios.get(
-      "http://pradekshaa-silks-backend.onrender.com/api/admin/products/get"
+      "https://pradekshaa-silks-backend.onrender.com/api/admin/products/get"
     );
 
     return result?.data;
@@ -38,7 +38,7 @@ export const editProduct = createAsyncThunk(
   "/products/editProduct",
   async ({ id, formData }) => {
     const result = await axios.put(
-      `http://pradekshaa-silks-backend.onrender.com/api/admin/products/edit/${id}`,
+      `https://pradekshaa-silks-backend.onrender.com/api/admin/products/edit/${id}`,
       formData,
       {
         headers: {
@@ -55,7 +55,7 @@ export const deleteProduct = createAsyncThunk(
   "/products/deleteProduct",
   async (id) => {
     const result = await axios.delete(
-      `http://pradekshaa-silks-backend.onrender.com/api/admin/products/delete/${id}`
+      `https://pradekshaa-silks-backend.onrender.com/api/admin/products/delete/${id}`
     );
 
     return result?.data;
